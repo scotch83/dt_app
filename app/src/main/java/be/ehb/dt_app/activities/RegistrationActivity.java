6dc762dae4edeb48778a2897249dd60614b47734
Lorenzvdv <lorenzvdv@hotmail.com>
2015-06-16 09:48:27 +0200

    Lorenz

--- app/src/main/java/be/ehb/dt_app/activities/RegistrationActivity.java
+++ app/src/main/java/be/ehb/dt_app/activities/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package be.ehb.dt_app.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,6 +8,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -16,14 +18,17 @@ import be.ehb.dt_app.R;
 import be.ehb.dt_app.controller.ZoomOutPageTransformer;
 import be.ehb.dt_app.fragments.RegistrationFragment;
 import be.ehb.dt_app.fragments.RegistrationFragment2;
+import be.ehb.dt_app.fragments.ScreensaverDialog;
 
 public class RegistrationActivity extends ActionBarActivity {
 
     protected Fragment form1, form2;
+    long lastUsed = System.currentTimeMillis();
+    boolean stopScreenSaver;
+    ScreensaverDialog screensaverDialog;
     private ViewPager mPagerRegistratie;
     private PagerAdapter mPagerAdapter;
     private ImageView img_page1, img_page2;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +78,9 @@ public class RegistrationActivity extends ActionBarActivity {
         });
 
         Toast.makeText(this, "Testing branches again!", Toast.LENGTH_LONG).show();
+        screensaverDialog = new ScreensaverDialog(RegistrationActivity.this, R.style.screensaver_dialog);
+        startScreensaverThread();
+
     }
 
     @Override
@@ -95,6 +103,60 @@ public class RegistrationActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        lastUsed = System.currentTimeMillis();
+    }
+
+    public void startScreensaverThread() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long idle = 0;
+                Log.d("started", "the screensaver has started");
+                do {
+                    idle = System.currentTimeMillis() - lastUsed;
+                    Log.d("something", "Application is idle for " + idle + " ms");
+
+                    if (idle > 5000) {
+                        if (!screensaverDialog.isShowing()) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    screensaverDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                                        @Override
+                                        public void onDismiss(DialogInterface dialog) {
+                                            stopScreenSaver = false;
+                                        }
+                                    });
+                                    screensaverDialog.show();
+                                }
+                            });
+                        } else {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    screensaverDialog.screensaverIV.setImageBitmap(screensaverDialog.bitmapArray.get(screensaverDialog.nextImage()));
+                                }
+                            });
+
+                        }
+                    }
+                    try {
+                        Thread.sleep(5000); //check every 5 seconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                while (!stopScreenSaver);
+            }
+
+        });
+        thread.start();
     }
 
     private class RegistratiePagerAdapter extends FragmentStatePagerAdapter {
