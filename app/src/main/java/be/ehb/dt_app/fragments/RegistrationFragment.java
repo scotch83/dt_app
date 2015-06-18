284f55d9394b718244163af72ef81bc94e51258d
ankecannaerts <anke.cannaerts@gmail.com>
2015-06-18 17:38:17 +0200

    Anke_layout

--- app/src/main/java/be/ehb/dt_app/fragments/RegistrationFragment.java
+++ app/src/main/java/be/ehb/dt_app/fragments/RegistrationFragment.java
@@ -11,15 +11,17 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import be.ehb.dt_app.R;
+import be.ehb.dt_app.aanvulling.Postcodezoeker;
 import be.ehb.dt_app.activities.RegistrationActivity;
 
 
 public class RegistrationFragment extends Fragment {
 
-    EditText emailET;
-    TextView emailTV;
-    LinearLayout llBottom;
-    ScrollView mScrollView;
+    private EditText emailET, postcodeET, voornaamET, achternaalET, straatnaamET, nummerET, stadET;
+    private TextView emailTV;
+    private LinearLayout llBottom;
+    private ScrollView mScrollView;
+    private Postcodezoeker postcodezoeker;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,6 +54,11 @@ public class RegistrationFragment extends Fragment {
 
         emailET = (EditText) v.findViewById(R.id.et_email);
         emailTV = (TextView) v.findViewById(R.id.tv_email);
+        postcodeET = (EditText) v.findViewById(R.id.et_postcode);
+        stadET = (EditText) v.findViewById(R.id.et_stad);
+
+        postcodezoeker = new Postcodezoeker(postcodeET, stadET, getActivity());
+
         llBottom = (LinearLayout) v.findViewById(R.id.ll_registratie1_bottom);
         mScrollView = (ScrollView) v.findViewById(R.id.sv_registratie1);
 
